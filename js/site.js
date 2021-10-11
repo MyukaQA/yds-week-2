1761bc28f3114b59311b7ef13ec3e16275a42fb2
MyukaQA <mylian214@gmail.com>
2021-10-11 08:55:01 +0700

    button to the top

--- js/site.js
+++ js/site.js
@@ -5,3 +5,22 @@ const nav_menu = document.getElementById('nav-menu');
 hamburger.addEventListener('click', () =>{
   nav_menu.classList.toggle('show');
 });
+
+var height_body = document.body.scrollHeight;
+var percent = 20;
+var btn_scroll = document.getElementById('btn-scroll');
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction(){
+  if (document.body.scrollTop > (height_body/100*percent) || document.documentElement.scrollTop > (height_body/100*percent)){
+    btn_scroll.style.display = 'block';
+  }else{
+    btn_scroll.style.display = 'none';
+  }
+}
+
+function btnTopFunction(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

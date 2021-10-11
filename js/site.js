643dfb13c3b998fd328c9a0f85e04fa22e3dc501
MyukaQA <mylian214@gmail.com>
2021-10-11 08:10:37 +0700

    menu hamburger

--- js/site.js
+++ js/site.js
@@ -1,1 +1,7 @@
 //kode javascript untuk index.html
+const hamburger = document.getElementById('hamburger');
+const nav_menu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () =>{
+  nav_menu.classList.toggle('show');
+});
